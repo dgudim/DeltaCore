d4cb35648007ab9c3831b14cdd4db61c9b210569
dgudim <dgudim@gmail.com>
2020-06-10 20:26:59 +0300

    desktop fullscreen

--- desktop/src/com/deo/flapd/desktop/DesktopLauncher.java
+++ desktop/src/com/deo/flapd/desktop/DesktopLauncher.java
@@ -12,7 +12,7 @@ public class DesktopLauncher {
 		config.height = 480;
 		config.title = "Deltacore";
 		config.allowSoftwareMode = true;
-		//config.fullscreen = true;
+		config.fullscreen = true;
 		config.vSyncEnabled = true;
 		new LwjglApplication(new Main(), config);
 	}
