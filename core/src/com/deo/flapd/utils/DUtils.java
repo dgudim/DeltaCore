647de292007abf08c03d3674054702d6b0eb20d7
kloud_xps13 <dgudim@gmail.com>
2021-09-23 16:41:08 +0300

    ...

--- core/src/com/deo/flapd/utils/DUtils.java
+++ core/src/com/deo/flapd/utils/DUtils.java
@@ -70,8 +70,7 @@ public class DUtils {
     }
     
     public static void clearLog() {
-        FileHandle file = Gdx.files.external(currentRootDir + "lastLog.txt");
-        file.writeString("", false);
+        logFile.writeString("", false);
         logBuffer = "";
         lastLine = "";
     }
