19f1b3330b7f0d9a7dfbeca980fbf0e92d8cc804
kloud_xps13 <dgudim@gmail.com>
2021-09-27 18:59:12 +0300

    Sound manager template

---
+++ core/src/com/deo/flapd/utils/SoundManager.java
@@ -0,0 +1,12 @@
+package com.deo.flapd.utils;
+
+public class SoundManager {
+    
+    String[] sounds = {"sfx/click.ogg", "sfx/explosion.ogg", "sfx/ftl.ogg", "sfx/ftl_flight.ogg",
+            "sfx/gun1.ogg", "sfx/gun2.ogg", "sfx/gun3.ogg", "sfx/gun4.ogg", "sfx/laser.ogg"};
+    
+    SoundManager(){
+    
+    }
+    
+}
