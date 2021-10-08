75f368199e2a76c54db4fff693ad0b2351d6c7c0
kloud <dgudim@gmail.com>
2021-10-08 15:26:36 +0300

    Empty class for new upgrade system

---
+++ core/src/com/deo/flapd/view/overlays/UpgradeMenu.java
@@ -0,0 +1,19 @@
+package com.deo.flapd.view.overlays;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.deo.flapd.utils.CompositeManager;
+
+public class UpgradeMenu {
+    
+    Button openButton;
+    Vector2 anchorPosition;
+    ShapeRenderer renderer;
+    
+    UpgradeMenu(CompositeManager compositeManager, Stage stage) {
+        Button.ButtonStyle style;
+    }
+    
+}
