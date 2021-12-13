57f1dbfa62f4ada3a2ad0235878023a6d5ece09a
kloud <dgudim@gmail.com>
2021-12-13 18:49:25 +0300

    Removed debug

--- core/src/com/deo/flapd/model/bullets/EnemyBullet.java
+++ core/src/com/deo/flapd/model/bullets/EnemyBullet.java
@@ -170,7 +170,6 @@ public class EnemyBullet extends Entity {
                         playerBullet.removeBullet(i, true);
                     }
                     health -= playerBulletHealth;
-                    System.out.println(health);
                     if(health <= 0){
                         explode();
                     }
